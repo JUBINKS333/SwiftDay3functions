1fa438d2d9321fd07c1a369daa55ca617488c5de
MacStudent <macstudent@126-imac29.local>
2019-02-11 18:57:37 -0500

    second

--- Day3Sample/main.swift
+++ Day3Sample/main.swift
@@ -12,3 +12,29 @@ print("Hello, World!")
 
 print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
+func hello()
+{
+    print("hello")
+}
+
+hello()
+
+func sum(a:Int, b:Int)
+{
+    let c = a + b;
+    print("sum:\(c)")
+}
+sum(a:3,b:4)
+
+// sum(b:3,a:4) will not work
+sum(a: 30, b: 40)
+
+//increasing readability
+func sum(of a:Float, and b:Float)
+{
+    let c = a + b;
+    print("sum -F :\(c)")
+}
+sum(of:10.5, and: 20.0)
+
+
